26c73a9483e5061161f68f5d59caa0ee10e3705a
Diana Olano <1005967885@u.icesi.edu.co>
2021-03-19 23:09:31 -0500

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
     
@@ -12,61 +12,44 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-       
-        String lineBooks = br.readLine(); 
-        int maxBooks=Integer.parseInt(lineBooks);
         
-        String booksPrice = br.readLine(); 
-        String[] booksPriceParts=booksPrice.split(" ");
-        int[] priceBooks=new int[maxBooks];
+        String finalResult="";
+        String lineBooks=br.readLine();
         
-        String peterMoney = br.readLine();
-        int money=Integer.parseInt(peterMoney);
-        
-        if(maxBooks>=2 && maxBooks<=10000){
-            for(int i=0;i<maxBooks;i++){
+        while(lineBooks!=null){
+            int maxBooks=Integer.parseInt(lineBooks);
+            int[] priceBooks=new int[maxBooks];
+            String[] booksPriceParts=br.readLine().split(" ");
+            
+            for(int i=0;i<booksPriceParts.length;i++){
                 priceBooks[i]=Integer.parseInt(booksPriceParts[i]);
             }
+            
+            int money=Integer.parseInt(br.readLine());
+            Arrays.sort(priceBooks);
+            int[] prices=searchBestPrices(priceBooks,money);
+           
+           finalResult+="Peter should buy books whose prices are "+prices[0]+" and "+prices[1]+"\n\n";
+           lineBooks=br.readLine();
+           lineBooks=br.readLine();
         }
-        
-        insertionSort(priceBooks);
-        int[] prices=searchBestPrices(priceBooks,money);
-        
-        bw.write("Peter should buy books whose prices are "+prices[0]+" and "+prices[1]);
-        bw.newLine();
-        
+        bw.write(finalResult);
         br.close();
         bw.close();
-    }
-    
-    public static int[] insertionSort(int[] arrayPrices){
-        for(int i=1;i<arrayPrices.length;i++){
-            for(int j=i;j>0 && arrayPrices[j-1]>arrayPrices[j];j--){
-                int temp=arrayPrices[j];
-                arrayPrices[j]=arrayPrices[j-1];
-                arrayPrices[j-1]=temp;
-            }
-        }
-        return arrayPrices;
     }
     
     public static int binarySearch(int[] bestpPriceBooks,int mainSubtraction){
         int i=0;
         int j=bestpPriceBooks.length-1;
         int pos=-1;
-
         while(i<=j && pos<0){
-            
             int m=(i+j)/2;
-            
             if(bestpPriceBooks[m]==mainSubtraction){
                 pos=m;
             }
-
             else if(bestpPriceBooks[m]>mainSubtraction){
                 j=m-1;
             }
-
             else{
                 i=m+1;
             }
@@ -76,20 +59,19 @@ public class Main {
     
     public static int[] searchBestPrices(int[] priceBooks,int peterPrice){
         int[] finalPrices=new int[2];
-        int minValue=Integer.MAX_VALUE;
-        
+        int initialValue=Integer.MAX_VALUE;
         for(int i=0;i<priceBooks.length;i++){
             int mainSubtraction=peterPrice-priceBooks[i];
             int foundBestPrices=binarySearch(priceBooks,mainSubtraction);
             int finalSubtraction=Math.abs(priceBooks[i]-mainSubtraction);
             
-            if(i!=foundBestPrices && foundBestPrices>=0 && finalSubtraction<minValue && finalSubtraction<MAXBOOKSPRICE){
-                minValue=finalSubtraction;
+            if(i!=foundBestPrices && foundBestPrices>=0 && finalSubtraction<initialValue && finalSubtraction<MAXBOOKSPRICE){
+                initialValue=finalSubtraction;
                 finalPrices[0]=priceBooks[i];
                 finalPrices[1]=mainSubtraction;
             }
         }
-        insertionSort(finalPrices);
+        Arrays.sort(finalPrices);
         return finalPrices;
     }
 }
